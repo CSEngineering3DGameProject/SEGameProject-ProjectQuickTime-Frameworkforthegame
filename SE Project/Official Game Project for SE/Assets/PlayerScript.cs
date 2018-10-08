f010e0e9b113f96a88fcd90aa61ca76fbfea16d7
coderguy1777 <hill.jordan@charterschool.org>
2018-10-08 17:55:27 -0400

    Collision Works, Kinda.

--- SE Project/Official Game Project for SE/Assets/PlayerScript.cs
+++ SE Project/Official Game Project for SE/Assets/PlayerScript.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float moveSpeed;
-
+    public Rigidbody rbody;
 
     void Start()
     {
         moveSpeed = 30f;
+        
     }
 
     void Update()
     {
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        
     }
 }
