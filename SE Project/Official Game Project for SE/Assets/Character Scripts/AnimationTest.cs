70e5076c3145477ef19e8d5630a191eb76a7f7a8
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-11-04 01:27:41 -0400

    Movement!

---
+++ SE Project/Official Game Project for SE/Assets/Character Scripts/AnimationTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour {
+
+    public Animator anim;
+    public float Test;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("space"))
+        {
+            anim.SetFloat("Test", Test - 1);
+        }
+        if(Input.GetKey("w")) 
+        {
+            anim.SetFloat("Test", Test + 1);
+            transform.Translate(0,0,5f);
+        }
+        if(Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -5f);
+        }
+	}
+}
---
+++ SE Project/Official Game Project for SE/Assets/Character Scripts/IdleToCrouch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToCrouch : MonoBehaviour {
+    public Animator animator;
+    public float inputX;
+    public float inputY;
+    public float Blend = 1f;
+    // Use this for initialization
+    void Start() {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey("Space"))
+        {
+            inputY = inputY - 1f;
+            animator.SetFloat("inputY", inputY);
+        }
+    }
+}
